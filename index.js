2bf5b661eb870d998e17de8a9dbbed77d63498fb
xunvyre <victoria@thedavids.us>
2022-08-17 16:04:20 -0400

    questions created and tested to make sure data passes correctly into the next functions

--- index.js
+++ index.js
@@ -1,13 +1,116 @@
 // TODO: Include packages needed for this application
 
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown");
+
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions =
+[
+    {
+        type: 'input',
+        name: 'username',
+        message: `What is your GitHub username?`,
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: `What is your preferred business email?`,
+    },
+    {
+        type: 'input',
+        name: 'title',
+        message: `What is the title of your project?`,
+    },
+    {
+        type: 'input',
+        name: 'desc',
+        message: `Please enter a description of your project:`,
+    },
+    {
+        type: 'confirm',
+        name: 'confirmInst',
+        message: `Will other people need installation instructions for your project?`,
+        default: true,
+    },
+    {
+        type: 'input',
+        name: 'inst',
+        message: `Please enter the installation instructions for your project:`,
+        when: ({confirmInst}) => confirmInst,
+    },
+    {
+        type: 'confirm',
+        name: 'confirmUse',
+        message: `Will other people need instruction on how to use your application?`,
+        default: true,
+    },
+    {
+        type: 'input',
+        name: 'use',
+        message: `Please enter the instructions for the use of your project:`,
+        when: ({confirmUse}) => confirmUse,
+    },
+    {
+        type: 'confirm',
+        name: 'conformCont',
+        message: `Would you like to include contribution instructions for your project?`,
+        default: true,
+    },
+    {
+        type: 'input',
+        name: 'cont',
+        message: `Please enter the contribution instructions for your project:`,
+        when: ({confirmCont}) => confirmCont,
+    },
+    {
+        type: 'confirm',
+        name: 'confirmTest',
+        message: `Would you like to include instructions for any tests included with your project?`,
+        default: false,
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: `Please enter the testing instructions for your project:`,
+        when: ({confirmTest}) => confirmTest,
+    },
+    {
+        type: 'confirm',
+        name: 'confirmLisence',
+        message: `Would you like to include lisencing information for your project on the README?`,
+        default: false,
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: `Please select a license:`,
+        choices: ['MIT', 'CC', 'GPL', 'MPL 2.0', 'WTFPL', 'Unlicense']
+    },
+    {
+        type: 'input',
+        name: 'credits',
+        message: `Please include your name and any other credits necessary for your project:`,
+    },
+    {
+        type: 'confirm',
+        name: 'toc',
+        message: `Would you like to include a table of contents in your README?`,
+        default: true,
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init()
+{
+    return inquirer.prompt(questions);
+};
 
 // Function call to initialize app
-init();
+init()
+    .then(data =>
+    {
+        generateMarkdown(data);
+    });
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,9 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license)
+{
+  "https://img.shields.io/badge/License-${license}-pink.svg"
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -12,9 +15,7 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
-
-`;
+  console.log(data);
 }
 
 module.exports = generateMarkdown;
