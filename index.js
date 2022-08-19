c1a254b267df268c6fbfd6ec407cc111a59f1431
xunvyre <victoria@thedavids.us>
2022-08-18 21:30:23 -0400

    writeFile copied from portfolio-generator

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // TODO: Include packages needed for this application
 
 const inquirer = require("inquirer");
+const writeFile = require("./utils/generateFile")
 const generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
@@ -125,4 +126,8 @@ init()
     {
         generateMarkdown(data);
         console.log(generateMarkdown(data));
+    })
+    .then(readmeData =>
+    {
+        return writeFile(readmeData);
     });
