a4b3fbea732044b6a1823afc5440b9fc67eef8c9
xunvyre <victoria@thedavids.us>
2022-08-18 21:06:05 -0400

    created and tested function for table of contents

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -17,5 +17,64 @@ Please reach out to me with any questions or suggestions!
 `;
 };
 
+//create the table of contents
+const generateTOC = data =>
+{
+  //if-else statements for defining functions for the toc return
+  if (data.toc === true)
+  {
+    if (data.confirmInst === true)
+    {
+      returnInst = () => {return `
+* [Installation](https://github.com/${data.username}/${data.repo}#installation)`};
+    }
+    else
+    {
+      returnInst = () => ""
+    }
+    if (data.confirmUse === true)
+    {
+      returnUse = () => {return `
+* [Usage](https://github.com/${data.username}/${data.repo}#usage)`};
+    }
+    else
+    {
+      returnUse = () => ""
+    }
+    if (data.confirmCont === true)
+    {
+      returnCont = () => {return `
+* [Contribution](https://github.com/${data.username}/${data.repo}#contribution)`};
+    }
+    else
+    {
+      returnCont = () => ""
+    }
+    if (data.confirmTest === true)
+    {
+      returnTest = () => {return `
+* [Testing](https://github.com/${data.username}/${data.repo}#contribution)`};
+    }
+    else
+    {
+      returnTest = () => ""
+    }
+    if (data.confirmLisence === true)
+    {
+      returnLisence = () => {return `
+* [Lisencing](https://github.com/${data.username}/${data.repo}#contribution)`};
+    }
+    else
+    {
+      returnLisence = () => ""
+    }
+  }
+  return `
+## Table of Contents
+${returnInst()}${returnUse()}
+* [Future Development](https://github.com/${data.username}/${data.repo}#future-development)${returnCont()}${returnTest()}${returnLisence()}
+* [Credits](https://github.com/${data.username}/${data.repo}#credits)`;
+};
+
 //export
 module.exports = generateMarkdown;
